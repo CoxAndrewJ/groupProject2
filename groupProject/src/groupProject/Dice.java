755ab61e5ed3bc2d04af3246c1398e0c98c44652
Andrew Cox <coxandrewj@gmail.com>
2021-07-21 17:08:08 -0600

    Update Dice.java

--- groupProject/src/groupProject/Dice.java
+++ groupProject/src/groupProject/Dice.java
@@ -4,14 +4,11 @@ public class Dice
 {
 
 	String hello;
-<<<<<<< HEAD
 	
 	private String canUpdate;
 	
-=======
 
 	String canupdate;
 	private int deeznutz = 2;
 
->>>>>>> ac10840b354c53115c81561d821a7586625bbd26
 }
