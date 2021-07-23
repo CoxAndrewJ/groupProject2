26b884e3c4ad35de0c76c985ff6872cd05e3f30b
Andrew Cox <coxandrewj@gmail.com>
2021-07-22 22:28:39 -0600

    Revert "Update Dice.java"
    This reverts commit 9803e8db69014cd3a502ed2c82b1fcd92181296c.

--- groupProject/src/groupProject/Dice.java
+++ groupProject/src/groupProject/Dice.java
@@ -1,21 +1,11 @@
 package groupProject;
 
-import java.util.Random;
-
 public class Dice
 {
-	
-	Random rand = new Random();
-	int sides = 6;
-	
-	Dice(){
-	}
 
-	public int roll() {
-		int roll = rand.nextInt(sides) +1;
-		return roll;
-	}
-	
-
+	String thisIsAChange;
+	private int d;
+	private int f;
+	private int deeznutz;
 
 }
